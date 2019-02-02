500c9028586638ef32c2bc5a50ee121cb77a487a
Janis Jansen <janis.jansen@gmx.de>
2019-02-02 20:40:37 +0100

    Added Auth/Login/Register service + page
    Service for user authentication and signIn/Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 
 import { AngularFireModule } from "angularfire2";
-import { AngularFirestoreModule, FirestoreSettingsToken } from "angularfire2/firestore";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "angularfire2/auth";
+
 import { environment } from "../environments/environment"
 
 @NgModule({
@@ -21,7 +23,8 @@ import { environment } from "../environments/environment"
     IonicModule.forRoot(),
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [
     StatusBar,
