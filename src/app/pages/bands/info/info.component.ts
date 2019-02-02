a6c43ad8bcd3aec26bb374b852a0ab3481788676
ixexel <xelexpro@gmail.com>
2019-02-02 16:36:35 +0100

    Fixed bands/info/info.component.ts
    Signed-off-by: ixexel <xelexpro@gmail.com>

--- src/app/pages/bands/info/info.component.ts
+++ src/app/pages/bands/info/info.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from "@angular/router";
-import { Festival, FestivalsService } from "../../../services/bands.service";
+import { Band, BandsService } from "../../../services/bands.service";
 
 
 @Component({
