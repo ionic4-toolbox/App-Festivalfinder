c7294a7efc31ae3cc1e3d95239e374ba3a520104
ixexel <xelexpro@gmail.com>
2019-02-03 12:51:59 +0100

    Added tab "Account" Added ngif in bands/info/info.component.html but it's not working.
    Signed-off-by: ixexel <xelexpro@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -12,7 +12,7 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
   ) {
     this.initializeApp();
   }
@@ -23,4 +23,5 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
 }
